87ac371002d089a0d8172f6b8ece22e3c158ddaa
Enrivtw <enriqueconcorreo@gmail.com>
2026-06-13 12:48:03 -0400

    ´´

--- js/script.js
+++ js/script.js
@@ -72,3 +72,13 @@ document.addEventListener('click', (event) => {
 window.addEventListener('popstate', () => {
     loadPage(window.location.pathname + window.location.search + window.location.hash, false);
 });
+
+//Para el botoncito del formulario de contacto
+document.getElementById('form-contacto').addEventListener('submit', function(e) {
+e.preventDefault();
+const btn = this.querySelector('.btn-enviar');
+btn.textContent = 'Mensaje enviado correctamente';
+btn.disabled = true;
+btn.style.background = '#48CAE4';
+btn.style.color = '#002554';
+});
