ed043c9674c5f3ff0d7aedad55fe1a8cb1cc2e8b
Adrian Rodriguez <adrix01x@gmail.com>
2026-06-15 15:53:56 -0400

    arreglar los enlaces por nose cuanta vez

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-fetch('/pages/header.html')
+fetch('/Apex/pages/header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header-container').innerHTML = data;
     });
 
-fetch('/pages/footer.html')
+fetch('/Apex/pages/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-container').innerHTML = data;
